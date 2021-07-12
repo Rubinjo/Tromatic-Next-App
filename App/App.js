7ab1bb963c87af98c56c238e729c53dd5837c6fb
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-07-12 13:04:57 +0200

    ✨ Redux store

--- App/App.js
+++ App/App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import AppNavigator from "./navigation/AppNavigator";
+import machineReducer from "./store/reducers/machine";
+import userReducer from "./store/reducers/user";
+
+const rootReducer = combineReducers({
+  machine: machineReducer,
+  user: userReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View>
+    <Provider store={store}>
       <StatusBar style="auto" />
       <AppNavigator />
-    </View>
+    </Provider>
   );
 }
 
---
+++ App/store/reducers/machine.js
@@ -0,0 +1,12 @@
+import Machine from "../../models/machine";
+
+const initialState = null;
+
+const machineReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default machineReducer;
