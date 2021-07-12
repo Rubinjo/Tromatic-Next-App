1dd8c24829dd80b43036662f61208a26a301d8b3
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-07-12 13:04:44 +0200

    ✨Basic data models

---
+++ App/models/user.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(id, companyId, fullName, role) {
+    this.id = id;
+    this.companyId = companyId;
+    this.fullName = fullName;
+    this.role = role;
+  }
+}
+
+export default User;
