091cb12d69fa8cde40e288fe9f980fd230446411
Ruben <ruben.lucas1997@gmail.com>
2021-07-12 13:04:57 +0200

    ✨ Redux store

---
+++ App/store/reducers/user.js
@@ -0,0 +1,12 @@
+import User from "../../models/user";
+
+const initialState = null;
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
