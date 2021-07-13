15d8d7f3c868b811bd420f56268af2909d59817b
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-07-13 22:41:24 +0200

    💄 Begin of styling navigation

--- App/navigation/AppNavigator.js
+++ App/navigation/AppNavigator.js
@@ -1,26 +1,73 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ConnectScreen from "../screens/ConnectScreen";
 import DetailsScreen from "../screens/DetailsScreen";
-import OverviewScreen from "../screens/OverviewScreen";
+import OverviewScreen, {
+  tabOptions as overviewTabOptions,
+} from "../screens/OverviewScreen";
 import ResetPasswordScreen from "../screens/ResetPasswordScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import SignInScreen from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 
-const defaultTabOptions = {};
+import COLORS from "../assets/constants/colors";
 
+// Krijg de kleuren enzo nog niet helemaal werkent
+// en krijg dat de juiste headers showen nog niet goed (momenteel heb je een header van de stack en van de tab navigators)
+
+const defaultStackOptions = {
+  headerStyle: {
+    backgroundColor: COLORS.PrimaryColor,
+  },
+  title: "",
+  headerTintColor: "white",
+};
+const defaultTabOptions = {
+  showLabel: false,
+};
+
+const LoginStack = createStackNavigator();
+const OverviewStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const OverviewStackNav = (props) => {
+  return (
+    <OverviewStack.Navigator screenOptions={defaultStackOptions}>
+      <OverviewStack.Screen name="Overview" component={OverviewScreen} />
+      <OverviewStack.Screen name="Details" component={DetailsScreen} />
+    </OverviewStack.Navigator>
+  );
+};
+
+// Create userSigned in variable that triggers different navigation stack
+// Below is simple dummy for testing
+const userSignedIn = true;
 
 const AppNavigator = (props) => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={defaultTabOptions}>
-        <Tab.Screen name="Overview" component={OverviewScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      {userSignedIn ? (
+        <Tab.Navigator screenOptions={defaultTabOptions}>
+          <Tab.Screen
+            name="OverviewStack"
+            component={OverviewStackNav}
+            options={overviewTabOptions}
+          />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      ) : (
+        <LoginStack.Navigator>
+          <LoginStack.Screen name="SignIn" component={SignInScreen} />
+          <LoginStack.Screen name="SignUp" component={SignUpScreen} />
+          <LoginStack.Screen
+            name="ResetPassword"
+            component={ResetPasswordScreen}
+          />
+        </LoginStack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
--- App/screens/OverviewScreen.js
+++ App/screens/OverviewScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const OverviewScreen = (props) => {
   return (
@@ -7,6 +8,15 @@ const OverviewScreen = (props) => {
       <Text>OverviewScreen!</Text>
     </View>
   );
+};
+
+// Hij is wit dus je ziet hem momenteel niet
+export const tabOptions = (navData) => {
+  return {
+    tabBarIcon: (props) => (
+      <MaterialCommunityIcons name="engine" size={props.size} color="white" />
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
