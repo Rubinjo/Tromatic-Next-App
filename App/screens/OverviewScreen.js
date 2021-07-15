d996708a7fb9df88efb45e635b7a1683f5438984
RickWiltvank <private-d1e6fdc6341a@example.invalid>
2021-07-15 19:59:54 +0200

    ✨ Detail screen shows title

--- App/screens/OverviewScreen.js
+++ App/screens/OverviewScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import {
   NavigationContainer,
   CommonActions,
@@ -10,10 +17,44 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Colors from "../assets/constants/colors";
 import useColorScheme from "react-native/Libraries/Utilities/useColorScheme";
 
+import { CHAMBERS } from "../data/dummy-data";
+
 const OverviewScreen = (props) => {
   const navigation = useNavigation();
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          props.navigation.navigate(
+            "Details",
+            {
+              itemId: itemData.item.id,
+              otherParam: "anything you want here",
+            }
+
+            // {
+            //   params: {
+            //     chamberId: itemData.item.id,
+            //     chamberTitle: itemData.item.title,
+            //   },
+            // }
+          )
+        }
+      >
+        <View style={styles.gridItem}>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={CHAMBERS}
+        renderItem={renderGridItem}
+      />
       <Text>OverviewScreen!</Text>
       <Button
         title="Go to details"
@@ -21,6 +62,10 @@ const OverviewScreen = (props) => {
       />
     </View>
   );
+};
+
+OverviewScreen.navigationOptions = {
+  headerTitle: "Overview",
 };
 
 // Hij is wit dus je ziet hem momenteel niet
@@ -42,6 +87,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 50,
   },
 });
 
