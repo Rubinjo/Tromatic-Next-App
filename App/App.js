dfe74f6a06fa63b9eae32849760665be16b5c514
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-08-23 16:23:30 +0200

    🔥 sqlite code

--- App/App.js
+++ App/App.js
@@ -9,7 +9,6 @@ import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-// import { initSettings, fetchSettings, insertLanguage } from "./database/sqlite";
 import AppNavigator from "./navigation/AppNavigator";
 import machineReducer from "./store/reducers/machine";
 import userReducer from "./store/reducers/user";
@@ -42,45 +41,6 @@ const persistor = persistStore(store);
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
-  // const [savedLanguage, setSavedLanguage] = useState([]);
-
-  // Initialize SQLite database
-  // async function initData() {
-  //   try {
-  //     await initSettings();
-  //   } catch (err) {
-  //     console.log("Initializing database failed.");
-  //     console.log(err);
-  //   }
-  // }
-
-  // Fetch data from sqlite database
-  // Save into context with the help of the useState savedShots
-  // async function fetchData() {
-  //   try {
-  //     let fetch = await fetchSettings();
-  //     let dataFetch = fetch.rows._array;
-  //     if (!dataFetch.length) {
-  //       await insertLanguage("English");
-  //       fetch = await fetchSettings();
-  //       dataFetch = status.rows._array;
-  //     }
-  //     setSavedLanguage(dataFetch[0].language);
-  //     console.log("Data fetched from database");
-  //   } catch (error) {
-  //     console.log("Fetching data from database failed");
-  //     console.log(error);
-  //   }
-  // }
-
-  // Execute database loading
-  // useEffect(() => {
-  //   async function loadDatabase() {
-  //     await initData();
-  //     await fetchData();
-  //   }
-  //   loadDatabase();
-  // }, []);
 
   if (!fontLoaded) {
     return (
