1c1002c5c37cb8a5019d9313e7ab5080bd70bc4b
Ruben <ruben.lucas1997@gmail.com>
2021-11-18 20:58:44 +0100

    ✨ Slider selector

--- App/screens/DetailsScreen.js
+++ App/screens/DetailsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import {
@@ -6,12 +6,16 @@ import {
   useRoute,
   useNavigation,
 } from "@react-navigation/native";
+import { Slider } from "@miblanchard/react-native-slider";
 // import { createNativeStackNavigator } from "@react-navigation/native-stack"; Uninstalled at this moment
 
 import LineChart from "../components/Chart";
 import { CHAMBERS } from "../data/dummy-data";
+import Config from "../utils/config";
+import Colors from "../assets/constants/colors";
 
 const DetailsScreen = (props) => {
+  const [value, setValue] = useState(1);
   // const navigation = useNavigation();
   const route = useRoute();
   const { itemId, otherParam } = route.params;
@@ -26,6 +30,19 @@ const DetailsScreen = (props) => {
       <View>
         <LineChart />
       </View>
+      <View style={styles.sliderContainer}>
+        <Slider
+          trackStyle={styles.track}
+          thumbStyle={styles.thumb}
+          maximumValue={5}
+          step={1}
+          value={value}
+          onValueChange={(value) => setValue(value)}
+          minimumTrackTintColor={Colors.PrimaryColor}
+          maximumTrackTintColor={Colors.SecondaryColor}
+        />
+      </View>
+      <Text>Time left: {value}</Text>
     </View>
   );
 };
@@ -36,6 +53,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  sliderContainer: {
+    width: Config.deviceWidth * 0.9,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  track: {
+    height: Config.deviceHeight * 0.015,
+    borderRadius: 6,
+  },
+  thumb: {
+    borderWidth: 1.5,
+    backgroundColor: "white",
   },
 });
 
