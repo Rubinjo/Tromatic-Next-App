d8d91b258c5fe4213a0a12bd20a04c5c274a532d
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-11-18 14:42:27 +0100

    ✨ Slider

--- App/components/CategoryGridTile.js
+++ App/components/CategoryGridTile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TouchableOpacity,
   View,
@@ -6,16 +6,33 @@ import {
   StyleSheet,
   TouchableNativeFeedback,
 } from "react-native";
+import { Slider } from "@miblanchard/react-native-slider";
 
 import Light from "./Light";
+import Config from "../utils/config";
+import Colors from "../assets/constants/colors";
 
 const CategoryGridTile = (props) => {
+  const [value, setValue] = useState(1);
   return (
     <View style={styles.gridItem}>
       <TouchableNativeFeedback style={{ flex: 1 }} onPress={props.onSelect}>
         <View style={{ ...styles.container, backgroundColor: props.color }}>
           <Light color={props.color} />
           <Text>{props.title}</Text>
+          <View style={styles.sliderContainer}>
+            <Slider
+              trackStyle={styles.track}
+              thumbStyle={styles.thumb}
+              maximumValue={100}
+              step={1}
+              value={value}
+              onValueChange={(value) => setValue(value)}
+              // disabled={true}
+              minimumTrackTintColor={Colors.SecondaryColor}
+            />
+          </View>
+          <Text>Time left: {value}</Text>
         </View>
       </TouchableNativeFeedback>
     </View>
@@ -27,7 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     width: 300,
-    height: 50,
+    height: 200,
     borderRadius: 10,
     overflow: "hidden",
   },
@@ -42,6 +59,27 @@ const styles = StyleSheet.create({
     padding: 15,
     justifyContent: "center",
     alignItems: "center",
+  },
+  sliderContainer: {
+    width: Config.deviceWidth * 0.7,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  track: {
+    height: Config.deviceWidth * 0.05,
+    borderRadius: 6,
+    backgroundColor: Colors.PrimaryColor,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    elevation: 2,
+  },
+  thumb: {
+    opacity: 0,
   },
 });
 
