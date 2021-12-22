bb49e7e283cd2e665c91d21e224ef24650e60e46
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-12-22 16:49:25 +0100

    🚚 Move Graph to own screen

---
+++ App/screens/GraphScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import LineChart from "../components/LineChart";
+
+const GraphScreen = () => {
+  return (
+    <View>
+      <Text>Graph Screen!</Text>
+      <LineChart />
+    </View>
+  );
+};
+
+export default GraphScreen;
