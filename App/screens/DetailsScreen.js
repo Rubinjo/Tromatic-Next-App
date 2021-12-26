f58e87c584ec8eae6cafd8fba120b2ce34901ac8
Ruben <ruben.lucas1997@gmail.com>
2021-12-26 14:44:16 +0100

    🎨 Basic styling

--- App/screens/DetailsScreen.js
+++ App/screens/DetailsScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 // import {
