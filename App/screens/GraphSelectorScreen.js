6796fecef381ca6f3a6d924655fe811846179e1f
Ruben <50332377+Rubinjo@users.noreply.github.com>
2021-12-26 14:44:16 +0100

    🎨 Basic styling

--- App/screens/GraphSelectorScreen.js
+++ App/screens/GraphSelectorScreen.js
@@ -1,14 +1,60 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Config from "../utils/config";
 
 const GraphSelectorScreen = (props) => {
+  const [isSelected, setIsSelected] = useState(0);
+
   return (
     <View>
-      <Text>Selector</Text>
+      <TouchableOpacity
+        onPress={() => setIsSelected(0)}
+        style={[styles.container, { borderTopWidth: 1 }]}
+      >
+        <Text style={styles.text}>Variable 1</Text>
+        {isSelected == 0 && (
+          <MaterialCommunityIcons
+            style={styles.icon}
+            name={"check"}
+            size={34}
+            color={"green"}
+          />
+        )}
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setIsSelected(1)}
+        style={styles.container}
+      >
+        <Text style={styles.text}>Variable 2</Text>
+        {isSelected == 1 && (
+          <MaterialCommunityIcons
+            style={styles.icon}
+            name={"check"}
+            size={34}
+            color={"green"}
+          />
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    paddingVertical: Config.deviceHeight * 0.002,
+  },
+  text: {
+    fontFamily: "noto-sans-jp-regular",
+    marginLeft: Config.deviceWidth * 0.04,
+  },
+  icon: {
+    marginLeft: "auto",
+    marginRight: Config.deviceWidth * 0.08,
+  },
+});
 
 export default GraphSelectorScreen;
