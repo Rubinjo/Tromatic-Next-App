eadb27bf11351f8de007bd380b46a2f4fefdda52
Ruben <ruben.lucas1997@gmail.com>
2021-12-29 21:30:27 +0100

    💥 Firebase machine data loading

--- App/models/machine.js
+++ App/models/machine.js
@@ -1,8 +1,10 @@
 class Machine {
-  constructor(id, companyId, type) {
+  constructor(id, companyId, type, creation, temperature) {
     this.id = id;
     this.companyId = companyId;
     this.type = type;
+    this.creation = creation;
+    this.temperature = temperature;
   }
 }
 
--- App/screens/OverviewScreen.js
+++ App/screens/OverviewScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -13,19 +13,59 @@ import {
   useNavigation,
 } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 import Colors from "../assets/constants/colors";
 
 import { CHAMBERS } from "../data/dummy-data";
+import Machine from "../models/machine";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 const OverviewScreen = (props) => {
-  const navigation = useNavigation();
+  const [cid, setCid] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const db = getDatabase();
+    const userRef = ref(db, "users/" + auth.currentUser.uid + "/cid");
+    onValue(userRef, (snapshot) => {
+      const cid = snapshot.val();
+      setCid(cid, true);
+    });
+    const machinesRef = ref(db, "companies/" + cid + "/machines");
+    onValue(machinesRef, (snapshot) => {
+      const fetchedData = [];
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        const parsRef = ref(db, "machines/" + childKey);
+        onValue(parsRef, (snapshot) => {
+          const parData = snapshot.val();
+          fetchedData.push(
+            new Machine(
+              childKey,
+              cid,
+              childData.type,
+              childData.creation,
+              parData.temperature
+            )
+          );
+        });
+      });
+      setData(fetchedData);
+    });
+    console.log(cid);
+    console.log(data);
+  }, []);
+  // const navigation = useNavigation();
+
   const renderGridItem = (itemData) => {
     return (
       <CategoryGridTile
         title={itemData.item.title}
-        color={itemData.item.color}
+        // color={itemData.item.color}
         onSelect={() => {
           props.navigation.navigate("Details", {
             itemId: itemData.item.id,
