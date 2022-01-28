de3f0e39d99f766dbadfc95bda0d5d594c54d7ca
Ruben Lucas <ruben.lucas1997@gmail.com>
2022-01-28 20:34:09 +0100

    🎨 protect route

--- Web/pages/companies.js
+++ Web/pages/companies.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { withProtected } from "../firebase/hook/route";
 
 function companies() {
   return <div></div>;
 }
 
-export default companies;
+export default withProtected(companies);
