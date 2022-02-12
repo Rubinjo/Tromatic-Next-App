5fbe08c80ec1c70e285b1cf73a413a593fb87113
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2022-02-12 11:24:54 +0100

    🎨 Basic layout DetailsScreen

--- App/screens/DetailsScreen.js
+++ App/screens/DetailsScreen.js
@@ -7,6 +7,7 @@ import {
   Switch,
   Platform,
   Image,
+  ScrollView,
 } from "react-native";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -87,8 +88,14 @@ const DetailsScreen = (props) => {
   // const { chamberId } = props.route.params;
   // const selectedChamber = CHAMBERS.find((chamId) => chamId.id === chamberId);
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, flexDirection: "row" }}>
+    <ScrollView style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          height: Config.deviceHeight * 0.35,
+        }}
+      >
         <View style={{ alignItems: "center" }}>
           <Image
             style={{
@@ -133,25 +140,374 @@ const DetailsScreen = (props) => {
         </View>
       </View>
 
-      <Switch
-        trackColor={{
-          true: Colors.SecondaryColor,
-          false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          marginBottom: -42,
         }}
-        thumbColor={
-          Platform.OS == "ios"
-            ? "#FFFFFF"
-            : m1Toggle
-            ? Colors.SecondaryColor
-            : "#ffffff"
-        }
-        ios_backgroundColor="#fbfbfb"
-        onValueChange={(value) => setM1Toggle(value)}
-        value={m1Toggle}
-        style={
-          m1Toggle ? styles.switchEnableBorder : styles.switchDisableBorder
-        }
-      />
+      >
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/valve.png")}
+            />
+            <Text>Valve</Text>
+          </View>
+          <View style={{ alignItems: "center", marginTop: 2 }}>
+            <Text>Auto</Text>
+            <Text>0%</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/heating.png")}
+            />
+            <Text>Heating</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>Auto</Text>
+            <Text>0%</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/sprinkler.png")}
+            />
+            <Text>Sprayer</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>Auto</Text>
+            <Text>0%</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center", height: 100 }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/directions.png")}
+            />
+            <Text>Direction</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>Auto</Text>
+            <Text>0%</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ borderBottomWidth: 1 }} />
+      <View style={{ borderBottomWidth: 1, marginTop: 38 }} />
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          marginBottom: -42,
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/atom.png")}
+            />
+            <Text>Core T1</Text>
+          </View>
+          <View style={{ alignItems: "center", marginTop: 2 }}>
+            <Text>23.7</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/atom.png")}
+            />
+            <Text>Core T2</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>10.6</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/atom.png")}
+            />
+            <Text>Core T3</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>15.7</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center", height: 100 }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/atom.png")}
+            />
+            <Text>Core T4</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>44.0</Text>
+          </View>
+        </View>
+        <View style={{ alignItems: "center", height: 100 }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{
+                height: 5 + Config.deviceHeight * 0.04,
+                width: 5 + Config.deviceHeight * 0.04,
+                resizeMode: "contain",
+              }}
+              source={require("../assets/icons/atom.png")}
+            />
+            <Text>5.6</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text>Auto</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ borderBottomWidth: 1 }} />
+      <View style={{ borderBottomWidth: 1, marginTop: 17 }} />
+      <View style={{ flexDirection: "row", width: "100%" }}>
+        <View style={{ flexDirection: "column", width: "50%" }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M1</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m1Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM1Toggle(value)}
+              value={m1Toggle}
+              style={
+                m1Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M2</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m2Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM2Toggle(value)}
+              value={m2Toggle}
+              style={
+                m2Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M3</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m3Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM3Toggle(value)}
+              value={m3Toggle}
+              style={
+                m3Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M4</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m4Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM4Toggle(value)}
+              value={m4Toggle}
+              style={
+                m4Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+        </View>
+        <View style={{ flexDirection: "column", width: "50%" }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M5</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m5Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM5Toggle(value)}
+              value={m5Toggle}
+              style={
+                m5Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M6</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m6Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM6Toggle(value)}
+              value={m6Toggle}
+              style={
+                m6Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M7</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m7Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM7Toggle(value)}
+              value={m7Toggle}
+              style={
+                m7Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text>M8</Text>
+            <Switch
+              trackColor={{
+                true: Colors.SecondaryColor,
+                false: Platform.OS == "android" ? "#d3d3d3" : "#fbfbfb",
+              }}
+              thumbColor={
+                Platform.OS == "ios"
+                  ? "#FFFFFF"
+                  : m8Toggle
+                  ? Colors.SecondaryColor
+                  : "#ffffff"
+              }
+              ios_backgroundColor="#fbfbfb"
+              onValueChange={(value) => setM8Toggle(value)}
+              value={m8Toggle}
+              style={
+                m8Toggle
+                  ? styles.switchEnableBorder
+                  : styles.switchDisableBorder
+              }
+            />
+          </View>
+        </View>
+      </View>
+
       {/* <SliderTile
         title="Valves"
         stepCount={[...Array(5).keys()]} // Array of steps (step length you want ++)
@@ -165,7 +521,7 @@ const DetailsScreen = (props) => {
       >
         <Text>See Graph</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -173,8 +529,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
+    // alignItems: "center",
     width: "100%",
   },
   switchEnableBorder: {
