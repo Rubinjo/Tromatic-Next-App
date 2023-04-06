12d77a770b7ae3e733021369f3f0829b4a896f6c
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-04-06 13:16:23 +0200

    ✨ Function to remove data >= 30 days

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -6,22 +6,40 @@ admin.initializeApp();
 const firebase = admin.database();
 const firestore = admin.firestore();
 
-exports.scheduledFunction = functions.region("europe-west1")
+exports.scheduledFbToFsFunction = functions.region("europe-west1")
     .runWith({maxInstances: 10})
     .pubsub
     .schedule("every 30 minutes")
-    .onRun((context) => {
-      get(ref(firebase, "machines")).then((snapshot) => {
-        const machines = snapshot.val();
-        const batch = firestore.batch();
-        for (const [key, value] of Object.entries(machines)) {
-          const machineRef = firestore.collection("machines")
-              .doc(key)
-              .collection("history")
-              .doc(new Date().toISOString());
-          batch.set(machineRef, value);
+    .onRun(async (context) => {
+      const snapshot = await get(ref(firebase, "machines"));
+      const machines = snapshot.val();
+      const batch = firestore.batch();
+      for (const [key, value] of Object.entries(machines)) {
+        const machineRef = firestore.collection("machines")
+            .doc(key)
+            .collection("history")
+            .doc(new Date().toISOString());
+        batch.set(machineRef, value);
+      }
+      await batch.commit();
+    });
+
+exports.scheduledRemoveOldDataFsFunction = functions.region("europe-west1")
+    .runWith({maxInstances: 10})
+    .pubsub
+    .schedule("every 30 minutes")
+    .onRun(async (context) => {
+      const now = new Date().getTime();
+      const machineRef = firestore.collectionGroup("history");
+      const snapshot = await machineRef.get();
+      const batch = firestore.batch();
+      snapshot.forEach((doc) => {
+        const timeDP = new Date(doc.id).getTime();
+        const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
+        const timeDiffInMs = now - timeDP;
+        if (timeDiffInMs >= thirtyDaysInMs) {
+          batch.delete(doc.ref);
         }
-        batch.commit();
       });
-      return null;
+      await batch.commit();
     });
