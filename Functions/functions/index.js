28dc51e69e8e91ff5ddd49feec9c37d5c93abd35
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-04-21 19:06:14 +0200

    🚑 Remove delete function for TTL feature

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -14,32 +14,38 @@ exports.scheduledFbToFsFunction = functions.region("europe-west1")
       const snapshot = await get(ref(firebase, "machines"));
       const machines = snapshot.val();
       const batch = firestore.batch();
+      const dateIdString = new Date().toISOString();
+      const dateExpiration = new Date();
+      dateExpiration.setDate(dateExpiration.getDate() + 30);
       for (const [key, value] of Object.entries(machines)) {
+        value["Expiration"] = dateExpiration;
         const machineRef = firestore.collection("machines")
             .doc(key)
             .collection("history")
-            .doc(new Date().toISOString());
+            .doc(dateIdString);
         batch.set(machineRef, value);
       }
       await batch.commit();
     });
 
-exports.scheduledRemoveOldDataFsFunction = functions.region("europe-west1")
-    .runWith({maxInstances: 10})
-    .pubsub
-    .schedule("every 30 minutes")
-    .onRun(async (context) => {
-      const now = new Date().getTime();
-      const machineRef = firestore.collectionGroup("history");
-      const snapshot = await machineRef.get();
-      const batch = firestore.batch();
-      snapshot.forEach((doc) => {
-        const timeDP = new Date(doc.id).getTime();
-        const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
-        const timeDiffInMs = now - timeDP;
-        if (timeDiffInMs >= thirtyDaysInMs) {
-          batch.delete(doc.ref);
-        }
-      });
-      await batch.commit();
-    });
+// DEPRECATED, replaced by firebase TTL policy
+// Not efficient at all, does lots of read requests
+// exports.scheduledRemoveOldDataFsFunction = functions.region("europe-west1")
+//     .runWith({maxInstances: 10})
+//     .pubsub
+//     .schedule("every 30 minutes")
+//     .onRun(async (context) => {
+//       const now = new Date().getTime();
+//       const machineRef = firestore.collectionGroup("history");
+//       const snapshot = await machineRef.get();
+//       const batch = firestore.batch();
+//       snapshot.forEach((doc) => {
+//         const timeDP = new Date(doc.id).getTime();
+//         const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
+//         const timeDiffInMs = now - timeDP;
+//         if (timeDiffInMs >= thirtyDaysInMs) {
+//           batch.delete(doc.ref);
+//         }
+//       });
+//       await batch.commit();
+//     });
