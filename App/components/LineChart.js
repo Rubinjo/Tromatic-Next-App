dbb3f85f1b7e1b60475e020f94a640e6a21e5b87
Rick Wiltvank <private-052b3d1477fb@example.invalid>
2023-04-25 20:02:42 +0200

    🎨 Test Prettier

--- App/components/LineChart.js
+++ App/components/LineChart.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
 import {
-  VictoryChart,
-  VictoryGroup,
-  VictoryAxis,
-  VictoryLine,
-  VictoryLegend,
-  VictoryTheme,
-  VictoryScatter,
-  LineSegment,
-  VictoryZoomContainer,
+	VictoryChart,
+	VictoryGroup,
+	VictoryAxis,
+	VictoryLine,
+	VictoryLegend,
+	VictoryTheme,
+	VictoryScatter,
+	LineSegment,
+	VictoryZoomContainer,
 } from "victory-native";
 import PropTypes from "prop-types";
 
@@ -17,11 +17,11 @@ import temperature from "../data/dummy-data-temp";
 import humidity from "../data/dummy-data-humi";
 
 const datastuff = temperature.forEach((element) => {
-  // do stuff with data
-  let x = element.x;
-  let y = element.y;
+	// do stuff with data
+	let x = element.x;
+	let y = element.y;
 
-  console.log(`${x}: ${y}`);
+	console.log(`${x}: ${y}`);
 });
 
 let last = temperature[temperature.length - 1];
@@ -50,66 +50,70 @@ console.log(Date());
 // }
 
 const LineChart = (props) => {
-  const datatemp = temperature;
-  const datahumi = humidity;
-  console.log(props.timer);
-  const timeAdjusted = 24 - props.timer;
-  return (
-    <View style={{ backgroundColor: "#ffffff" }}>
-      <View style={styles.titlebox}>
-        <Text style={{}}>Variables over time</Text>
-      </View>
+	const datatemp = temperature;
+	const datahumi = humidity;
+	console.log(props.timer);
+	const timeAdjusted = 24 - props.timer;
+	return (
+		<View style={{ backgroundColor: "#ffffff" }}>
+			<View style={styles.titlebox}>
+				<Text style={{}}>Variables over time</Text>
+			</View>
 
-      <VictoryChart
-        gridComponent={<LineSegment type={"grid"} />}
-        minDomain={{ x: timeAdjusted }}
-        style={{}}
-      >
-        <VictoryGroup style={{}}>
-          <VictoryLine
-            style={{
-              data: { stroke: "#c43a31" },
-              parent: { border: "1px solid #ccc" },
-            }}
-            domain={{ y: [0, 100] }} //x=24
-            data={datatemp}
-          ></VictoryLine>
-          <VictoryLine
-            style={{
-              data: { stroke: "#5eb1bf" },
-              parent: { border: "1px solid #ccc" },
-            }}
-            data={datahumi}
-          ></VictoryLine>
-          {/* <VictoryScatter
+			<VictoryChart
+				gridComponent={<LineSegment type={"grid"} />}
+				minDomain={{ x: timeAdjusted }}
+				style={{}}
+			>
+				<VictoryGroup style={{}}>
+					<VictoryLine
+						style={{
+							data: { stroke: "#c43a31" },
+							parent: { border: "1px solid #ccc" },
+						}}
+						domain={{ y: [0, 100] }} //x=24
+						data={datatemp}
+					></VictoryLine>
+					<VictoryLine
+						style={{
+							data: { stroke: "#5eb1bf" },
+							parent: { border: "1px solid #ccc" },
+						}}
+						data={datahumi}
+					></VictoryLine>
+					{/* <VictoryScatter
             dataComponent={<CatPoint />}
             y={(d) => Math.sin(2 * Math.PI * d.x)}
             samples={15}
           /> */}
-        </VictoryGroup>
-        <VictoryAxis label="Time [h]" scale={{ x: "time" }} style={{}} />
-        <VictoryAxis
-          dependentAxis
-          style={{}}
-          // label="Temperature [°C]"
-          // style={{
-          //   axisLabel: {
-          //     padding: 30,
-          //   },
-          // }}
-        />
-        <VictoryAxis
-          style={{
-            grid: { stroke: "#818e99", strokeWidth: 0.5 },
-          }}
-        />
-        <VictoryAxis
-          dependentAxis
-          style={{
-            grid: { stroke: "#818e99", strokeWidth: 0.5 },
-          }}
-        />
-        {/* <VictoryZoomContainer
+				</VictoryGroup>
+				<VictoryAxis
+					label="Time [h]"
+					scale={{ x: "time" }}
+					style={{}}
+				/>
+				<VictoryAxis
+					dependentAxis
+					style={{}}
+					// label="Temperature [°C]"
+					// style={{
+					//   axisLabel: {
+					//     padding: 30,
+					//   },
+					// }}
+				/>
+				<VictoryAxis
+					style={{
+						grid: { stroke: "#818e99", strokeWidth: 0.5 },
+					}}
+				/>
+				<VictoryAxis
+					dependentAxis
+					style={{
+						grid: { stroke: "#818e99", strokeWidth: 0.5 },
+					}}
+				/>
+				{/* <VictoryZoomContainer
           responsive={false}
           zoomDimension="x"
           zoomDomain={this.state.zoomDomain}
@@ -121,42 +125,42 @@ const LineChart = (props) => {
           brushDomain={this.state.selectedDomain}
           onBrushDomainChange={this.handleBrush.bind(this)}
         /> */}
-        <VictoryLegend
-          x={Dimensions.get("screen").width / 2 - 100}
-          orientation="horizontal"
-          data={[
-            {
-              name: "Temperature",
-              symbol: {
-                fill: "#c43a31",
-              },
-            },
-            {
-              name: "Humidity",
-              symbol: {
-                fill: "#5eb1bf",
-              },
-            },
-          ]}
-          style={{}}
-        />
-      </VictoryChart>
-      <View alignItems="center">
-        <Text style={{}}>
-          The machine has been running for: {last.x}h and is at{" "}
-          {last.y.toFixed(2)}°C
-        </Text>
-      </View>
-    </View>
-  );
+				<VictoryLegend
+					x={Dimensions.get("screen").width / 2 - 100}
+					orientation="horizontal"
+					data={[
+						{
+							name: "Temperature",
+							symbol: {
+								fill: "#c43a31",
+							},
+						},
+						{
+							name: "Humidity",
+							symbol: {
+								fill: "#5eb1bf",
+							},
+						},
+					]}
+					style={{}}
+				/>
+			</VictoryChart>
+			<View alignItems="center">
+				<Text style={{}}>
+					The machine has been running for: {last.x}h and is at{" "}
+					{last.y.toFixed(2)}°C
+				</Text>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  titlebox: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	titlebox: {
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
 
 export default LineChart;
