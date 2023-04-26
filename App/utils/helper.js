0794c48bacd77b8dfcd673dc12c66a1571c2d7f5
Rick Wiltvank <private-052b3d1477fb@example.invalid>
2023-04-26 16:25:25 +0200

    💄 Small todo point

--- App/utils/helper.js
+++ App/utils/helper.js
@@ -1,61 +1,67 @@
 function fancyTimeFormat(duration) {
-    const days = ~~(duration / 24);
-    const hrs = ~~(duration % 24);
+	const days = ~~(duration / 24);
+	const hrs = ~~(duration % 24);
 
-    let ret = "";
+	let ret = "";
+	// TODO: convert strings to i18n
+	if (days > 0) {
+		ret += days + " days ";
+	}
 
-    if (days > 0) {
-        ret += days + " days ";
-    }
-
-    ret += hrs + " hours";
-    return ret;
+	ret += hrs + " hours";
+	return ret;
 }
 
 function directionTitle(direction) {
-    if (direction == 64) {
-        return "←"
-    } else if (direction == 32) {
-        return "→"
-    } else if (Number.isInteger(direction)) {
-        return "o"
-    } else {
-        return "Error"
-    }
+	if (direction == 64) {
+		return "←";
+	} else if (direction == 32) {
+		return "→";
+	} else if (Number.isInteger(direction)) {
+		return "o";
+	} else {
+		return "Error";
+	}
 }
 
 function directionValue(direction) {
-    if (direction === 64) {
-        return 1
-    } else if (direction === 32) {
-        return 0
-    } else {
-        return 0.5
-    }
+	if (direction === 64) {
+		return 1;
+	} else if (direction === 32) {
+		return 0;
+	} else {
+		return 0.5;
+	}
 }
 
 function opModeTitle(opMode) {
-    if (opMode === 0) {
-        return "Auto"
-    } else if (opMode == 1) {
-        return "Manual"
-    } else {
-        return "Error"
-    }
+	if (opMode === 0) {
+		return "Auto";
+	} else if (opMode == 1) {
+		return "Manual";
+	} else {
+		return "Error";
+	}
 }
 
 function opModeFanTitle(opMode) {
-    if (opMode === 0) {
-        return "Auto"
-    } else if (opMode == 1) {
-        return "On"
-    } else if (opMode == 2) {
-        return "Off"
-    } else if (opMode == 3) {
-        return "Manual"
-    } else {
-        return "Error"
-    }
+	if (opMode === 0) {
+		return "Auto";
+	} else if (opMode == 1) {
+		return "On";
+	} else if (opMode == 2) {
+		return "Off";
+	} else if (opMode == 3) {
+		return "Manual";
+	} else {
+		return "Error";
+	}
 }
 
-export { fancyTimeFormat, directionTitle, directionValue, opModeTitle, opModeFanTitle }
+export {
+	fancyTimeFormat,
+	directionTitle,
+	directionValue,
+	opModeTitle,
+	opModeFanTitle,
+};
