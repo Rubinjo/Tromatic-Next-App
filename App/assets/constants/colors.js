42701129765723310aa445334e34d64a10a3a63c
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-05-05 22:04:00 +0200

    💄 Warning color

--- App/assets/constants/colors.js
+++ App/assets/constants/colors.js
@@ -2,6 +2,7 @@ export default {
 	Primary: "#0F7BCA",
 	Secondary: "#1AA3FF",
 	Active: "#00CC63",
+	Warning: "#FFFF00",
 	NotActive: "#FF1755",
 	PrimaryDark: "#000000",
 	SecondaryDark: "#707070",
