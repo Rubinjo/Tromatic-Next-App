f404a55ab2eee29ba95de9d8c4a8f9ccf8313d05
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-05-05 22:44:15 +0200

    🎨 Get CurrentWM value in sender

--- comms-js/sender.js
+++ comms-js/sender.js
@@ -31,15 +31,19 @@ let md5Previous = null;
 let fsWait = false;
 let jsonData, jsonString, md5Current;
 
-const [db, auth] = await setupFirebase({
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-}, process.env.EMAIL, process.env.PASSWORD);
+const [db, auth] = await setupFirebase(
+	{
+		apiKey: process.env.APIKEY,
+		authDomain: process.env.AUTHDOMAIN,
+		databaseURL: process.env.DATABASEURL,
+		projectId: process.env.PROJECTID,
+		storageBucket: process.env.STORAGEBUCKET,
+		messagingSenderId: process.env.MESSAGINGSENDERID,
+		appId: process.env.APPID,
+	},
+	process.env.EMAIL,
+	process.env.PASSWORD
+);
 
 /**
  * Send drychamber values to firebase constantly.
@@ -49,62 +53,94 @@ const [db, auth] = await setupFirebase({
  * @param {string} filename - Name of file that has changed.
  */
 fs.watch(FOLDER, (event, filename) => {
-  if (filename.split(".").pop() === "json") {
-    if (fsWait) return;
-    // Debounce function
-    // Protection against a file triggering multiple times for a single action
-    fsWait = setTimeout(() => {
-      fsWait = false;
-    }, 100);
-    // Use MD5 hash for checksum
-    // Extra protection against a file triggering multiple times for a single action
-    md5Current = md5(fs.readFileSync(FOLDER + "/" + filename));
-    if (md5Current === md5Previous) {
-      return;
-    }
-    md5Previous = md5Current;
-    log.info(`${filename} file recorded`, 0)
-    try {
-      jsonString = fs.readFileSync(FOLDER + "/" + filename);
-      jsonData = JSON.parse(jsonString)
-      const updates = {};
-      updates["machines/m" + auth.currentUser.uid + "_" + jsonData.DryChamberID] = {
-        DateTimeMessage: jsonData.DateTimeMessage,
-        Status: jsonData.Status,
-        CurrentTemp: jsonData.CurrentTemp,
-        SetPointTemp: jsonData.SetPointTemp,
-        CurrentHum: jsonData.CurrentHum,
-        SetPointHum: jsonData.SetPointHum,
-        RemainingTime: jsonData.RemainingTime,
-        TotalTime: jsonData.TotalTime,
-        NumOfWmProbes: jsonData.NumOfWmProbes,
-        HeatingValvePos: jsonData.HeatingValvePos,
-        DamperPos: jsonData.DamperPos,
-        SprayPos: jsonData.SprayPos,
-        RPM: jsonData.RPM,
-        FanDirection: jsonData.FanDirection,
-        TempOffset: jsonData.TempOffset,
-        EMCOffset: jsonData.EMCOffset,
-        NumOfCTProbes: jsonData.NumOfCTProbes,
-        DamperOpMode: jsonData.DamperOpMode,
-        HeaterOpMode: jsonData.HeaterOpMode,
-        SprayOpMode: jsonData.SprayOpMode,
-        FansOpMode: jsonData.FansOpMode,
-        LastEditor: "m" + auth.currentUser.uid + "_" + jsonData.DryChamberID,
-        DeviceName: jsonData.DeviceName
-      }
-      for (let i = 1; i <= jsonData.NumOfWmProbes; i++) {
-        updates["machines/m" + auth.currentUser.uid + "_" + jsonData.DryChamberID]["WMValue" + i] = jsonData["WMValue" + i];
-        updates["machines/m" + auth.currentUser.uid + "_" + jsonData.DryChamberID]["WMActive" + i] = jsonData["WMActive" + i];
-      }
-      for (let i = 1; i <= jsonData.NumOfCTProbes; i++) {
-        updates["machines/m" + auth.currentUser.uid + "_" + jsonData.DryChamberID]["CTValue" + i] = jsonData["CTValue" + i];
-      }
-      update(ref(db), updates);
-    } catch (e) {
-      log.error(e, 58)
-    }
-  } else {
-    log.warn(`${filename} file has no .json extension and is ignored`, 1630);
-  }
+	if (filename.split(".").pop() === "json") {
+		if (fsWait) return;
+		// Debounce function
+		// Protection against a file triggering multiple times for a single action
+		fsWait = setTimeout(() => {
+			fsWait = false;
+		}, 100);
+		// Use MD5 hash for checksum
+		// Extra protection against a file triggering multiple times for a single action
+		md5Current = md5(fs.readFileSync(FOLDER + "/" + filename));
+		if (md5Current === md5Previous) {
+			return;
+		}
+		md5Previous = md5Current;
+		log.info(`${filename} file recorded`, 0);
+		try {
+			jsonString = fs.readFileSync(FOLDER + "/" + filename);
+			jsonData = JSON.parse(jsonString);
+			const updates = {};
+			updates[
+				"machines/m" +
+					auth.currentUser.uid +
+					"_" +
+					jsonData.DryChamberID
+			] = {
+				DateTimeMessage: jsonData.DateTimeMessage,
+				Status: jsonData.Status,
+				CurrentTemp: jsonData.CurrentTemp,
+				SetPointTemp: jsonData.SetPointTemp,
+				CurrentHum: jsonData.CurrentHum,
+				SetPointHum: jsonData.SetPointHum,
+				RemainingTime: jsonData.RemainingTime,
+				TotalTime: jsonData.TotalTime,
+				NumOfWmProbes: jsonData.NumOfWmProbes,
+				HeatingValvePos: jsonData.HeatingValvePos,
+				DamperPos: jsonData.DamperPos,
+				SprayPos: jsonData.SprayPos,
+				RPM: jsonData.RPM,
+				FanDirection: jsonData.FanDirection,
+				TempOffset: jsonData.TempOffset,
+				EMCOffset: jsonData.EMCOffset,
+				NumOfCTProbes: jsonData.NumOfCTProbes,
+				DamperOpMode: jsonData.DamperOpMode,
+				HeaterOpMode: jsonData.HeaterOpMode,
+				SprayOpMode: jsonData.SprayOpMode,
+				FansOpMode: jsonData.FansOpMode,
+				LastEditor:
+					"m" + auth.currentUser.uid + "_" + jsonData.DryChamberID,
+				DeviceName: jsonData.DeviceName,
+			};
+			for (let i = 1; i <= jsonData.NumOfWmProbes; i++) {
+				updates[
+					"machines/m" +
+						auth.currentUser.uid +
+						"_" +
+						jsonData.DryChamberID
+				]["WMValue" + i] = jsonData["WMValue" + i];
+				updates[
+					"machines/m" +
+						auth.currentUser.uid +
+						"_" +
+						jsonData.DryChamberID
+				]["WMActive" + i] = jsonData["WMActive" + i];
+			}
+			for (let i = 1; i <= jsonData.NumOfCTProbes; i++) {
+				updates[
+					"machines/m" +
+						auth.currentUser.uid +
+						"_" +
+						jsonData.DryChamberID
+				]["CTValue" + i] = jsonData["CTValue" + i];
+			}
+			if (jsonData.CurrentWM) {
+				updates[
+					"machines/m" +
+						auth.currentUser.uid +
+						"_" +
+						jsonData.DryChamberID
+				]["CurrentWM"] = jsonData.CurrentWM;
+			}
+			update(ref(db), updates);
+		} catch (e) {
+			log.error(e, 58);
+		}
+	} else {
+		log.warn(
+			`${filename} file has no .json extension and is ignored`,
+			1630
+		);
+	}
 });
