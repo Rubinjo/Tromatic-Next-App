4f8fd706da33e3bbbac70fc0a58f19e755bb2fde
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-06-14 22:06:55 +0200

    ✨ Create and Save expoPushToken in firebase

--- App/App.js
+++ App/App.js
@@ -1,17 +1,16 @@
-import "react-native-gesture-handler"
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React, { useCallback, useEffect, useState } from "react";
 import Constants from "expo-constants";
 import { View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
-import * as Font from 'expo-font';
+import * as Font from "expo-font";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth"
-import { getReactNativePersistence } from "firebase/auth/react-native"
+import { initializeAuth } from "firebase/auth";
+import { getReactNativePersistence } from "firebase/auth/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as Notifications from 'expo-notifications';
 
 import AppNavigator from "./navigation/AppNavigator";
 import { store, persistor } from "./store/store";
@@ -21,73 +20,64 @@ SplashScreen.preventAutoHideAsync();
 
 // Fetch custom font-family
 const fetchFonts = async () => {
-  return Font.loadAsync({
-    "noto-sans-jp-regular": require("./assets/fonts/NotoSansJP-Regular.otf"),
-    "noto-sans-jp-bold": require("./assets/fonts/NotoSansJP-Bold.otf"),
-  });
+	return Font.loadAsync({
+		"noto-sans-jp-regular": require("./assets/fonts/NotoSansJP-Regular.otf"),
+		"noto-sans-jp-bold": require("./assets/fonts/NotoSansJP-Bold.otf"),
+	});
 };
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
+	const [appIsReady, setAppIsReady] = useState(false);
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await fetchFonts();
-        console.log("Loaded fonts");
+	useEffect(() => {
+		async function prepare() {
+			try {
+				await fetchFonts();
+				console.log("Loaded fonts");
 
-        const app = initializeApp({
-          apiKey: Constants.expoConfig.extra.APIKEY,
-          authDomain: Constants.expoConfig.extra.AUTHDOMAIN,
-          databaseURL: Constants.expoConfig.extra.DATABASEURL,
-          projectId: Constants.expoConfig.extra.PROJECTID,
-          storageBucket: Constants.expoConfig.extra.STORAGEBUCKET,
-          messagingSenderId: Constants.expoConfig.extra.MESSAGINGSENDERID,
-          appId: Constants.expoConfig.extra.APPID,
-        });
-        initializeAuth(app, {
-          persistence: getReactNativePersistence(AsyncStorage)
-        });
-        console.log("Connected with Firebase");
+				const app = initializeApp({
+					apiKey: Constants.expoConfig.extra.APIKEY,
+					authDomain: Constants.expoConfig.extra.AUTHDOMAIN,
+					databaseURL: Constants.expoConfig.extra.DATABASEURL,
+					projectId: Constants.expoConfig.extra.PROJECTID,
+					storageBucket: Constants.expoConfig.extra.STORAGEBUCKET,
+					messagingSenderId:
+						Constants.expoConfig.extra.MESSAGINGSENDERID,
+					appId: Constants.expoConfig.extra.APPID,
+				});
+				initializeAuth(app, {
+					persistence: getReactNativePersistence(AsyncStorage),
+				});
+				console.log("Connected with Firebase");
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true);
+			}
+		}
 
-        // Initialize local notification
-        // Notifications.setNotificationHandler({
-        //   handleNotification: async () => ({
-        //     shouldShowAlert: true,
-        //     shouldPlaySound: true,
-        //     shouldSetBadge: true,
-        //   }),
-        // });
-        // console.log("Initialized notification service")
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
+		prepare();
+	}, []);
 
-    prepare();
-  }, []);
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
+	if (!appIsReady) {
+		return null;
+	}
 
-  if (!appIsReady) {
-    return null;
-  }
-
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-          <StatusBar style="auto" />
-          <AppNavigator />
-        </View>
-      </PersistGate>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+					<StatusBar style="auto" />
+					<AppNavigator />
+				</View>
+			</PersistGate>
+		</Provider>
+	);
 }
--- App/screens/OverviewScreen.js
+++ App/screens/OverviewScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, SafeAreaView, FlatList, View } from "react-native";
 import {
 	NavigationContainer,
@@ -6,7 +6,9 @@ import {
 	useNavigation,
 } from "@react-navigation/native";
 import { getAuth } from "firebase/auth";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, update } from "firebase/database";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 import Gauge from "../assets/icons/Gauge";
 import Config from "../utils/config";
@@ -16,8 +18,92 @@ import Machine from "../models/machine";
 import Status from "../models/status";
 import CategoryGridTile from "../components/CategoryGridTile";
 
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
+
+async function registerForPushNotificationsAsync() {
+	let token;
+
+	if (Platform.OS === "android") {
+		await Notifications.setNotificationChannelAsync("default", {
+			name: "default",
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: "#FF231F7C",
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } =
+			await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== "granted") {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== "granted") {
+			alert("Failed to get push token for push notification!");
+			return;
+		}
+		token = (await Notifications.getExpoPushTokenAsync()).data;
+		console.log(token);
+	} else {
+		alert("Must use physical device for Push Notifications");
+	}
+
+	return token;
+}
+
 const OverviewScreen = (props) => {
 	const [data, setData] = useState([]);
+	const [expoPushToken, setExpoPushToken] = useState("");
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then((token) =>
+			setExpoPushToken(token)
+		);
+
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				setNotification(notification);
+			});
+
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener(
+				(response) => {
+					console.log(response);
+				}
+			);
+
+		return () => {
+			Notifications.removeNotificationSubscription(
+				notificationListener.current
+			);
+			Notifications.removeNotificationSubscription(
+				responseListener.current
+			);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (expoPushToken) {
+			const db = getDatabase();
+			const auth = getAuth();
+
+			update(ref(db, `users/${auth.currentUser.uid}`), {
+				expoPushToken: expoPushToken,
+			});
+		}
+	}, [expoPushToken]);
+
 	useEffect(() => {
 		const auth = getAuth();
 		const db = getDatabase();
