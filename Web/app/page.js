0afa2ed809359b36031afad1aa112ca79164c782
Rick <private-052b3d1477fb@example.invalid>
2023-09-04 22:19:17 +0200

    📃Todo

--- Web/app/page.js
+++ Web/app/page.js
@@ -2,6 +2,21 @@ export default function Home() {
 	return (
 		<main>
 			<h1>Under development by RTR</h1>
+		<!--
+		// LOGIN PAGINA
+		// ...
+		// Hoe hoog ben je in de ranking?
+		// Admin pagina:
+		// Company pagina (hoeveel kan je zien door je ranking?):
+			// Users pagina
+			// Machine pagina
+		// ...
+		// Technische dingen:
+			// App is heilig
+			// App kan nieuwe roles / users / machines / companies krijgen door de website
+			// Comms connecten
+				// AKA hardware connectie
+		-->
 		</main>
 	);
 }
