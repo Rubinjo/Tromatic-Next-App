cf93f16a39bf06540b3b3e9043f56b1f66733001
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-09-19 21:46:19 +0200

    💄 Style Tab Component

--- Web/components/Tab.js
+++ Web/components/Tab.js
@@ -5,8 +5,34 @@ import Link from "next/link";
 export default function Tab({ selection }) {
 	return (
 		<div>
-			<Link href="/models/machines">Machines</Link>
-			<Link href="/models/users">Users</Link>
+			<ul class="hidden text-sm font-medium text-center shadow sm:flex">
+				<li class="w-full">
+					<Link
+						href="/models/machines"
+						class={
+							"inline-block w-full p-4 rounded-l-lg focus:ring-2 focus:outline-none " +
+							(selection === "machines"
+								? "active text-white bg-primary"
+								: "text-gray-500 bg-black-100 hover:text-gray-700 hover:bg-gray-50")
+						}
+					>
+						Machines
+					</Link>
+				</li>
+				<li class="w-full">
+					<Link
+						href="/models/users"
+						class={
+							"inline-block w-full p-4 rounded-r-lg focus:ring-2 focus:outline-none " +
+							(selection === "users"
+								? "active text-white bg-primary"
+								: "text-gray-500 bg-black-100 hover:text-gray-700 hover:bg-gray-50")
+						}
+					>
+						Users
+					</Link>
+				</li>
+			</ul>
 		</div>
 	);
 }
