4d142e6a3bea526d13d2aba5e445631d2f519d87
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-09-19 21:46:31 +0200

    💄 Style Table Component

--- Web/components/Table.js
+++ Web/components/Table.js
@@ -7,6 +7,7 @@ import {
 	getSortedRowModel,
 	getFilteredRowModel,
 } from "@tanstack/react-table";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Table({ data, columns }) {
 	const [sorting, setSorting] = useState([]);
@@ -26,39 +27,61 @@ export default function Table({ data, columns }) {
 		onGlobalFilterChange: setFiltering,
 	});
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Search"
-				value={filtering}
-				onChange={(e) => setFiltering(e.target.value)}
-			/>
-			<table>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<th
-								key={header.id}
-								onClick={header.column.getToggleSortingHandler()}
-							>
-								{flexRender(
-									header.column.columnDef.header,
-									header.getContext()
-								)}
-								{
-									{ asc: "🔼", desc: "🔽" }[
-										header.column.getIsSorted() ?? null
-									]
-								}
-							</th>
-						))}
-					</tr>
-				))}
+		<div class="relative overflow-x-auto sm:rounded">
+			<div class="flex p-2 justify-end">
+				<label for="table-search" class="sr-only">
+					Search
+				</label>
+				<div class="relative mt-1">
+					<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+						<FaMagnifyingGlass
+							class="w-4 h-4 text-gray-500 dark:text-gray-400"
+							aria-hidden="true"
+						/>
+					</div>
+					<input
+						id="table-search"
+						type="text"
+						placeholder="Search"
+						class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-secondary focus:border-secondary"
+						value={filtering}
+						onChange={(e) => setFiltering(e.target.value)}
+					/>
+				</div>
+			</div>
+			<table class="w-full text-sm text-left shadow-md">
+				<thead class="w-full text-xs text-white uppercase bg-primary">
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th
+									key={header.id}
+									onClick={header.column.getToggleSortingHandler()}
+									scope="col"
+									class="px-6 py-3"
+								>
+									{flexRender(
+										header.column.columnDef.header,
+										header.getContext()
+									)}
+									{
+										{ asc: "🔼", desc: "🔽" }[
+											header.column.getIsSorted() ?? null
+										]
+									}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
 				<tbody>
 					{table.getRowModel().rows.map((row) => (
-						<tr key={row.id}>
+						<tr
+							key={row.id}
+							class="bg-white border-b text-gray-700"
+						>
 							{row.getVisibleCells().map((cell) => (
-								<td key={cell.id}>
+								<td key={cell.id} class="px-6 py-4">
 									{flexRender(
 										cell.column.columnDef.cell,
 										cell.getContext()
@@ -69,23 +92,39 @@ export default function Table({ data, columns }) {
 					))}
 				</tbody>
 			</table>
-			<div>
-				<button onClick={() => table.setPageIndex(0)}>
+			<div class="text-center flex justify-center text-sm">
+				<button
+					class="px-4 py-2 my-4 shadow text-gray-700 hover:text-gray-900 hover:bg-gray-50 rounded-l"
+					onClick={() => table.setPageIndex(0)}
+				>
 					First page
 				</button>
 				<button
+					class={
+						"px-4 py-2 my-4 shadow text-gray-700 " +
+						(!table.getCanPreviousPage()
+							? "bg-gray-200"
+							: "hover:text-gray-900 hover:bg-gray-50")
+					}
 					disabled={!table.getCanPreviousPage()}
 					onClick={() => table.previousPage()}
 				>
 					Previous page
 				</button>
 				<button
+					class={
+						"px-4 py-2 my-4 shadow text-gray-700 " +
+						(!table.getCanNextPage()
+							? "bg-gray-200"
+							: "hover:text-gray-900 hover:bg-gray-50")
+					}
 					disabled={!table.getCanNextPage()}
 					onClick={() => table.nextPage()}
 				>
 					Next page
 				</button>
 				<button
+					class="px-4 py-2 my-4 shadow text-gray-700 hover:text-gray-900 hover:bg-gray-50 rounded-r"
 					onClick={() => table.setPageIndex(table.getPageCount() - 1)}
 				>
 					Last page
