765d55c51565dd8f92be374b3fe3b4b7949ad64b
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2023-11-14 18:06:37 +0100

    🐛 Handle multiple updates simultaneously

--- Comms-js/receiver.js
+++ Comms-js/receiver.js
@@ -7,37 +7,82 @@ import setupFirebase from "./helper/auth.js";
 
 dotenv.config();
 
+const valuesToWatch = [
+	"RemainingTime",
+	"TempOffset",
+	"EMCOffset",
+	"WMActive1",
+	"WMActive2",
+	"WMActive3",
+	"WMActive4",
+	"WMActive5",
+	"WMActive6",
+	"WMActive7",
+	"WMActive8",
+	"WMActive9",
+];
+
 const log = new EventLogger("Tromatic Next Receiver");
 
 let mids;
 
-function sendData(mid, dateTime, lastEditor, changeItem, changeValue) {
+function sendData(mid, dateTime, lastEditor, changedDict) {
 	const machineData = {
 		DryChamberID: mid,
 		DateTimeMessage: dateTime.toISOString(),
 		LastEditor: lastEditor,
 	};
-	if (changeItem === "RemainingTime") {
+	if (changedDict.hasOwnProperty("RemainingTime")) {
 		machineData["stop_program"] = true;
-	} else {
-		machineData[changeItem] = changeValue;
+		delete changedDict["RemainingTime"];
 	}
-	const jsonString = JSON.stringify(machineData, null, " ");
+	const jsonString = JSON.stringify(
+		{ ...machineData, ...changedDict },
+		null,
+		" "
+	);
 	fs.writeFile(
-		"../receiver/" +
-			mid +
-			"_" +
-			dateTime.toJSON().slice(0, 19).replaceAll(":", "-") +
-			".json",
+		`../receiver/${mid}_${dateTime
+			.toJSON()
+			.slice(0, 19)
+			.replaceAll(":", "-")}.json`,
 		jsonString,
 		function (e) {
 			if (e) {
 				log.error(e, 82);
 			} else {
-				log.info(mid + ".json was added/updated", 0);
+				log.info(`${mid}.json was added/updated`, 0);
 			}
 		}
 	);
+}
+
+// Function to get the keys with changed values
+function getChangedValues(previousData, currentData) {
+	const changedValues = {};
+
+	for (const key in currentData) {
+		if (
+			currentData.hasOwnProperty(key) &&
+			previousData[key] !== currentData[key]
+		) {
+			changedValues[key] = currentData[key];
+		}
+	}
+
+	return changedValues;
+}
+
+function filterDictionaryByKeys(dictionary) {
+	const result = {};
+
+	for (const key of valuesToWatch) {
+		if (key in dictionary) {
+			result[key] = dictionary[key];
+		}
+	}
+
+	return result;
 }
 
 const [db, auth] = await setupFirebase(
@@ -55,64 +100,47 @@ const [db, auth] = await setupFirebase(
 );
 
 try {
-	onValue(
-		ref(db, "companies/" + process.env.CID + "/machines"),
-		(snapshot) => {
-			const machines = snapshot.val();
-			mids = Object.keys(machines);
-			for (const mid of mids) {
-				for (const changeItem of [
-					"RemainingTime",
-					"TempOffset",
-					"EMCOffset",
-					"WMActive1",
-					"WMActive2",
-					"WMActive3",
-					"WMActive4",
-					"WMActive5",
-					"WMActive6",
-					"WMActive7",
-					"WMActive8",
-					"WMActive9",
-				]) {
-					onValue(
-						ref(db, "machines/" + mid + "/" + changeItem),
-						async (snapshot) => {
-							const changeValue = snapshot.val();
-							const lastEditor = (
-								await get(
-									ref(db, "machines/" + mid + "/LastEditor")
-								)
+	onValue(ref(db, `companies/${process.env.CID}/machines`), (snapshot) => {
+		const machines = snapshot.val();
+		mids = Object.keys(machines);
+		for (const mid of mids) {
+			let previousMachineData = null;
+			onValue(ref(db, `machines/${mid}`), async (snapshot) => {
+				const machineData = snapshot.val();
+				// Check if this is not the first invocation
+				if (previousMachineData !== null) {
+					const lastEditor = machineData.LastEditor;
+					if (lastEditor.startsWith("u")) {
+						const changedValues = getChangedValues(
+							previousMachineData,
+							machineData
+						);
+						const filteredData =
+							filterDictionaryByKeys(changedValues);
+						if (Object.keys(filteredData).length !== 0) {
+							const uid = lastEditor
+								.split("_")
+								.slice(0)[0]
+								.slice(1);
+							const fullName = (
+								await get(ref(db, `users/${uid}/fullName`))
 							).val();
-							if (lastEditor.startsWith("u")) {
-								const uid = lastEditor
-									.split("_")
-									.slice(0)[0]
-									.slice(1);
-								const fullName = (
-									await get(
-										ref(db, "users/" + uid + "/fullName")
-									)
-								).val();
-								sendData(
-									mid.split("_").slice(-1)[0],
-									new Date(),
-									fullName,
-									changeItem,
-									changeValue
-								);
-							} else {
-								log.info(
-									"Registered update was not from user",
-									0
-								);
-							}
+							sendData(
+								mid.split("_").slice(-1)[0],
+								new Date(),
+								fullName,
+								filteredData
+							);
 						}
-					);
+					} else {
+						log.info("Registered update was not from user", 0);
+					}
 				}
-			}
+				// Update the previous state with the current state for the next invocation
+				previousMachineData = machineData;
+			});
 		}
-	);
+	});
 } catch (e) {
 	log.error(e, 58);
 }
