80e1cf03bc178d2cb622e712d853f353775eab27
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2024-03-14 12:28:55 +0100

    🐛 Check if user already has __session cookie

--- Web/middleware.js
+++ Web/middleware.js
@@ -1,17 +1,21 @@
 import createMiddleware from "next-intl/middleware";
 
 export default function middleware(req) {
-	const localeServer = JSON.parse(req.cookies.get("__session").value);
+	const localeServer = req.cookies.get("__session")
+		? JSON.parse(req.cookies.get("__session").value)
+		: null;
 
 	// Set cookie on request so that next-intl can use it
-	req.cookies.set({
-		name: "NEXT_LOCALE",
-		value: localeServer.NEXT_LOCALE,
-		// httpOnly: true,
-		maxAge: 31536000,
-		path: "/",
-		sameSite: "lax",
-	});
+	if (localeServer) {
+		req.cookies.set({
+			name: "NEXT_LOCALE",
+			value: localeServer.NEXT_LOCALE,
+			// httpOnly: true,
+			maxAge: 31536000,
+			path: "/",
+			sameSite: "lax",
+		});
+	}
 
 	const handleI18nRouting = createMiddleware({
 		// A list of all locales that are supported
@@ -25,14 +29,16 @@ export default function middleware(req) {
 	const res = handleI18nRouting(req);
 
 	// Set cookie on response so it is also saved on the client
-	res.cookies.set({
-		name: "NEXT_LOCALE",
-		value: localeServer.NEXT_LOCALE,
-		// httpOnly: true,
-		maxAge: 31536000,
-		path: "/",
-		sameSite: "lax",
-	});
+	if (localeServer) {
+		res.cookies.set({
+			name: "NEXT_LOCALE",
+			value: localeServer.NEXT_LOCALE,
+			// httpOnly: true,
+			maxAge: 31536000,
+			path: "/",
+			sameSite: "lax",
+		});
+	}
 
 	return res;
 }
