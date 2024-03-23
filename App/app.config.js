840bc07b9e36a7966a994c5a95a2444b60b2c7f1
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2024-03-23 10:14:23 +0100

    🔖 Update version num

--- App/app.config.js
+++ App/app.config.js
@@ -10,58 +10,58 @@ const appId = process.env.APPID;
 const googleServiceFile = process.env.GOOGLE_SERVICES_JSON;
 
 export default {
-	name: "Tromatic NEXT",
-	slug: "Tromatic-NEXT",
-	owner: "your-expo-owner",
-	currentFullName: "tromatic-next",
-	version: "1.0.14",
-	platforms: ["ios", "android"],
-	orientation: "portrait",
-	primaryColor: "#0F7BCA",
-	icon: "./assets/icon.png",
-	splash: {
-		image: "./assets/splash.png",
-		resizeMode: "contain",
-		backgroundColor: "#0F7BCA",
-	},
-	updates: {
-		fallbackToCacheTimeout: 0,
-		url: "https://u.expo.dev/00000000-0000-4000-8000-000000000000",
-	},
-	runtimeVersion: {
-		policy: "sdkVersion",
-	},
-	assetBundlePatterns: ["**/*"],
-	notification: {
-		icon: "./assets/notification-icon.png",
-		color: "#0F7BCA",
-		iosDisplayInForeground: true,
-	},
-	ios: {
-		supportsTablet: true,
-		buildNumber: "1.0.14",
-		bundleIdentifier: "nl.bes-bollmann.tromatic-next",
-	},
-	android: {
-		package: "nl.bes_bollmann.tromatic_next",
-		googleServicesFile: googleServiceFile,
-		adaptiveIcon: {
-			foregroundImage: "./assets/adaptive-icon.png",
-			backgroundColor: "#0F7BCA",
-		},
-		softwareKeyboardLayoutMode: "pan",
-		versionCode: 15,
-	},
-	extra: {
-		APIKEY: apiKey,
-		AUTHDOMAIN: authDomain,
-		DATABASEURL: databaseURL,
-		PROJECTID: projectId,
-		STROAGEBUCKET: storageBucket,
-		MESSAGINGSENDERID: messagingSenderId,
-		APPID: appId,
-		eas: {
-			projectId: "00000000-0000-4000-8000-000000000000",
-		},
-	},
+    name: "Tromatic NEXT",
+    slug: "Tromatic-NEXT",
+    owner: "your-expo-owner",
+    currentFullName: "tromatic-next",
+    version: "1.0.15",
+    platforms: ["ios", "android"],
+    orientation: "portrait",
+    primaryColor: "#0F7BCA",
+    icon: "./assets/icon.png",
+    splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#0F7BCA",
+    },
+    updates: {
+        fallbackToCacheTimeout: 0,
+        url: "https://u.expo.dev/00000000-0000-4000-8000-000000000000",
+    },
+    runtimeVersion: {
+        policy: "sdkVersion",
+    },
+    assetBundlePatterns: ["**/*"],
+    notification: {
+        icon: "./assets/notification-icon.png",
+        color: "#0F7BCA",
+        iosDisplayInForeground: true,
+    },
+    ios: {
+        supportsTablet: true,
+        buildNumber: "1.0.15",
+        bundleIdentifier: "nl.bes-bollmann.tromatic-next",
+    },
+    android: {
+        package: "nl.bes_bollmann.tromatic_next",
+        googleServicesFile: googleServiceFile,
+        adaptiveIcon: {
+            foregroundImage: "./assets/adaptive-icon.png",
+            backgroundColor: "#0F7BCA",
+        },
+        softwareKeyboardLayoutMode: "pan",
+        versionCode: 16,
+    },
+    extra: {
+        APIKEY: apiKey,
+        AUTHDOMAIN: authDomain,
+        DATABASEURL: databaseURL,
+        PROJECTID: projectId,
+        STROAGEBUCKET: storageBucket,
+        MESSAGINGSENDERID: messagingSenderId,
+        APPID: appId,
+        eas: {
+            projectId: "00000000-0000-4000-8000-000000000000",
+        },
+    },
 };
