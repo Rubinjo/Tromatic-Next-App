ccc1081cdb0746475aeb56737cdd565a8a36628d
Ruben Lucas <50332377+Rubinjo@users.noreply.github.com>
2024-07-29 12:38:00 +0200

    🔖 Update release version

--- App/app.config.js
+++ App/app.config.js
@@ -14,7 +14,7 @@ export default {
     slug: "Tromatic-NEXT",
     owner: "your-expo-owner",
     currentFullName: "tromatic-next",
-    version: "1.0.17",
+    version: "1.0.18",
     platforms: ["ios", "android"],
     orientation: "portrait",
     primaryColor: "#0F7BCA",
@@ -39,7 +39,7 @@ export default {
     },
     ios: {
         supportsTablet: true,
-        buildNumber: "1.0.17",
+        buildNumber: "1.0.18",
         bundleIdentifier: "nl.bes-bollmann.tromatic-next",
     },
     android: {
@@ -50,7 +50,7 @@ export default {
             backgroundColor: "#0F7BCA",
         },
         softwareKeyboardLayoutMode: "pan",
-        versionCode: 18,
+        versionCode: 19,
     },
     plugins: ["expo-localization"],
     extra: {
